4eb358a69351d740d7834e97b52fb14bcd1476ea
Rogers Setiady <setiady.rogers28@gmail.com>
2021-04-03 23:31:03 +0700

    username/password tidak boleh kosong

--- application/controllers/api/Users.php
+++ application/controllers/api/Users.php
@@ -13,17 +13,25 @@ class Users extends RestController {
 
     public function login_post()
     {
-        $this->db->select('id_user, email_user, nama_user, nomor_handphone');
-        $this->db->where('email_user', $this->post('email'));
-        $this->db->where('password_user', md5($this->post('password')));
-        $query = $this->db->get('users');
-        if($query->num_rows() == 1){
-            $user = $query->row_array();
-            $this->response(array(
-                'status' => 'true',
-                'msg' => 'ok',
-                'data' => $user
-            ), 200 );
+        if($this->post('email') != '' && $this->post('password') != ''){
+            $this->db->select('id_user, email_user, nama_user, nomor_handphone');
+            $this->db->where('email_user', $this->post('email'));
+            $this->db->where('password_user', md5($this->post('password')));
+            $query = $this->db->get('users');
+            if($query->num_rows() == 1){
+                $user = $query->row_array();
+                $this->response(array(
+                    'status' => 'true',
+                    'msg' => 'ok',
+                    'data' => $user
+                ), 200 );
+            }
+            else {
+                $this->response(array(
+                    'status' => 'false',
+                    'msg' => 'Login Gagal'
+                ), 200 );
+            }
         }
         else {
             $this->response(array(
@@ -35,40 +43,48 @@ class Users extends RestController {
 
     public function registrasi_post()
     {
-        //cek email
-        $this->db->where('email_user', $this->post('email'));
-        $query = $this->db->get('users');
-        if($query->num_rows() == 0){
-            //cek no handphone
-            $this->db->where('nomor_handphone', $this->post('nomor_handphone'));
+        if($this->post('email') != '' && $this->post('password') != '' && $this->post('nama') != '' && $this->post('nomor_handphone') != ''){
+            //cek email
+            $this->db->where('email_user', $this->post('email'));
             $query = $this->db->get('users');
             if($query->num_rows() == 0){
-                $users = array(
-                    'email_user' => $this->post('email'),
-                    'password_user' => md5($this->post('password')),
-                    'nama_user' => $this->post('nama'),
-                    'nomor_handphone' => $this->post('nomor_handphone'),
-                    'saldo_user' => 0
-                );
-        
-                $this->db->insert('users', $users);
+                //cek no handphone
+                $this->db->where('nomor_handphone', $this->post('nomor_handphone'));
+                $query = $this->db->get('users');
+                if($query->num_rows() == 0){
+                    $users = array(
+                        'email_user' => $this->post('email'),
+                        'password_user' => md5($this->post('password')),
+                        'nama_user' => $this->post('nama'),
+                        'nomor_handphone' => $this->post('nomor_handphone'),
+                        'saldo_user' => 0
+                    );
+            
+                    $this->db->insert('users', $users);
 
-                $this->response(array(
-                    'status' => 'true',
-                    'msg' => 'ok'
-                ), 200 );
+                    $this->response(array(
+                        'status' => 'true',
+                        'msg' => 'ok'
+                    ), 200 );
+                }
+                else {
+                    $this->response(array(
+                        'status' => 'false',
+                        'msg' => 'Nomor Handphone Sudah Terdaftar'
+                    ), 200 );
+                }
             }
             else {
                 $this->response(array(
                     'status' => 'false',
-                    'msg' => 'Nomor Handphone Sudah Terdaftar'
+                    'msg' => 'Email Sudah Terdaftar'
                 ), 200 );
             }
         }
         else {
             $this->response(array(
                 'status' => 'false',
-                'msg' => 'Email Sudah Terdaftar'
+                'msg' => 'Pendaftaran Gagal, Email/Password/Nama/No Handphone Tidak Boleh Kosong'
             ), 200 );
         }
     }
